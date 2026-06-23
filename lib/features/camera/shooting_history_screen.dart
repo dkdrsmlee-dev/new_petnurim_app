11b8acfe7f3d5fc0494fda55a35b385d5bce6eee
smlee <dkdrsmlee@gmail.com>
2026-06-23 13:48:23 +0900

    feat(camera): apply custom home and back SVG icons from Figma

--- lib/features/camera/shooting_history_screen.dart
+++ lib/features/camera/shooting_history_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../core/widgets/camera_history_card.dart';
 import '../../core/widgets/pet_select_card.dart';
 import 'camera_screen.dart';
@@ -41,10 +42,10 @@ class ShootingHistoryScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios_new_rounded,
-            size: 20,
-            color: Color(0xFF30343C),
+          icon: SvgPicture.string(
+            _backIconSvg,
+            width: 24,
+            height: 24,
           ),
           onPressed: () => Navigator.pop(context),
         ),
@@ -61,10 +62,10 @@ class ShootingHistoryScreen extends StatelessWidget {
         centerTitle: true,
         actions: [
           IconButton(
-            icon: const Icon(
-              Icons.home_outlined,
-              size: 24,
-              color: Color(0xFF30343C),
+            icon: SvgPicture.string(
+              _homeIconSvg,
+              width: 24,
+              height: 24,
             ),
             onPressed: () {
               // 홈 화면으로 바로 이동 (모든 스택 팝 후 홈 탭 복귀 등)
@@ -182,3 +183,25 @@ class _EmptyHistoryView extends StatelessWidget {
     );
   }
 }
+
+// 피그마 원본 Icon/Home/24 벡터 패스 (굴뚝 디테일 포함)
+const String _homeIconSvg = '''
+<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M11.3789 3.71573C11.7426 3.42779 12.2574 3.42779 12.6211 3.71573L19.6211 9.25772C19.8605 9.4474 20 9.73645 20 10.0419V18.9491C20 19.5014 19.5523 19.9491 19 19.9491H5C4.44772 19.9491 4 19.5014 4 18.9491V10.0419C4 9.73645 4.1395 9.4474 4.37891 9.25772L11.3789 3.71573Z" stroke="#51565F" stroke-width="2"/>
+  <g transform="translate(12, 15)">
+    <path d="M2 1C2 0.447715 1.55228 0 1 0C0.447715 0 0 0.447715 0 1H1H2ZM1 7H2V1H1H0V7H1Z" fill="#51565F"/>
+  </g>
+</svg>
+''';
+
+// 피그마 원본 Icon/ArrowLeft/24-2 벡터 패스 (긴 뒤로가기 화살표)
+const String _backIconSvg = '''
+<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g transform="translate(4, 6) rotate(180 3.5 6)">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C0.683418 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L6.70711 5.29289C7.09763 5.68342 7.09763 6.31658 6.70711 6.70711L1.70711 11.7071C1.31658 12.0976 0.683418 12.0976 0.292893 11.7071C-0.0976311 11.3166 -0.0976311 10.6834 0.292893 10.2929L4.58579 6L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683418 0.292893 0.292893Z" fill="#51565F"/>
+  </g>
+  <g transform="translate(5, 11)">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M0 1C0 0.447715 0.447715 0 1 0H14C14.5523 0 15 0.447715 15 1C15 1.55228 14.5523 2 14 2H1C0.447715 2 0 1.55228 0 1Z" fill="#51565F"/>
+  </g>
+</svg>
+''';
